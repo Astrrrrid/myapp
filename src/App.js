e6fbe59c621cefc4a1791d77bdc70886faaf2807
Astrid Wang <astrid@Astrids-MBP.lan1>
2020-08-08 14:40:08 -0400

    page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,4 +23,33 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                
+                <p>
+                    welcome to Astrid's demo page. 
+        </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+        </a>
+            </header>
+        </div>
+    );
+}
+
+export default App; 
+
+
